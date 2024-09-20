732256811ede59e655ac3a6aa582cc9f4b519b95
Arina <terekhinaarina00@gmail.com>
2024-09-20 20:44:43 +0500

    добавить функционал для фулл скрина

--- src/js/sections/viewer/events/click.js
+++ src/js/sections/viewer/events/click.js
@@ -1,11 +1,13 @@
 import { handleViewPanorama } from '../handle_viewer_panorama.js';
 import { toggleClassName } from '../../../modules/functions.js';
 import { toggleClassToAlter } from '../../alter_parent_window.js';
+const parentWindow = window.parent;
 
 export function click(viewerInstance) {
   const viewerSlideExt = document.querySelector(".viewer__swiper .swiper-slide.ext");
   const viewerSlideInt = document.querySelector(".viewer__swiper .swiper-slide.int");
   const initialActiveSlide = viewerSlideExt ? viewerSlideExt : viewerSlideInt;
+
 
   const setViewPanorama = handleViewPanorama(viewerInstance);
 
@@ -23,6 +25,7 @@ export function click(viewerInstance) {
 };
 
 function handleFakeFullscreen(el) {
+  parentWindow.document.body.classList.toggle("fakeFullscreenActive")
   toggleClassName(document.body, 'fake-fullscreen-active');
   toggleClassToAlter();
 }
