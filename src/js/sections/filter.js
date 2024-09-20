997058600909d81727eb843fc6fb8413053c18f8
Quandiq <quandiqkalbaev@gmail.com>
2024-09-20 11:46:23 +0500

    filter remove active from html

--- src/js/sections/filter.js
+++ src/js/sections/filter.js
@@ -1,9 +1,16 @@
-export default function filter() {
+/** @format */
+
+export default function filter(selectedType) {
   const switchItems = document.querySelectorAll(".masterplan__switch-item");
   const contentItems = document.querySelectorAll("g[data-type]");
-  const checkboxBlock = document.querySelector(".masterplan__checkbox"); 
-  const checkboxItems= document.querySelectorAll(".masterplan__checkbox-item");
+  const checkboxBlock = document.querySelector(".masterplan__checkbox");
+  const checkboxItems = document.querySelectorAll(".masterplan__checkbox-item");
   const checkboxValues = document.querySelectorAll("g[data-value]");
+  const selectedCheckbox = document.querySelector(`g[data-value="${selectedType}"]`);
+  const selectedItem = document.querySelector(`.masterplan__checkbox-item[data-value="${selectedType}"]`);
+  
+  selectedCheckbox.classList.add("active");
+  selectedItem.classList.add("active");
 
 
   switchItems.forEach((item) => {
@@ -24,9 +31,9 @@ export default function filter() {
       }
     });
   });
+
   checkboxItems.forEach((item) => {
     item.addEventListener("click", function () {
-      
       checkboxItems.forEach((el) => el.classList.remove("active"));
       this.classList.add("active");
       const selectedType = this.getAttribute("data-value");
@@ -34,12 +41,6 @@ export default function filter() {
       document.querySelectorAll(`g[data-value="${selectedType}"]`).forEach((el) => {
         el.classList.add("active");
       });
-
-      // if (selectedType === "exterior") {
-      //   checkboxBlock.classList.add("hide");
-      // } else {
-      //   checkboxBlock.classList.remove("hide");
-      // }
     });
   });
 }
