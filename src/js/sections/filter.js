73aae01a6cae32cd9dd91b97fd63fbb8939bfcd8
Quandiq <quandiqkalbaev@gmail.com>
2024-09-20 13:13:56 +0500

    optimize js code & fix bug

--- src/js/sections/filter.js
+++ src/js/sections/filter.js
@@ -2,44 +2,50 @@
 
 export default function filter(selectedType) {
   const switchItems = document.querySelectorAll(".masterplan__switch-item");
-  const contentItems = document.querySelectorAll("g[data-type]");
   const checkboxBlock = document.querySelector(".masterplan__checkbox");
   const checkboxItems = document.querySelectorAll(".masterplan__checkbox-item");
   const checkboxValues = document.querySelectorAll("g[data-value]");
   const selectedCheckbox = document.querySelector(`g[data-value="${selectedType}"]`);
   const selectedItem = document.querySelector(`.masterplan__checkbox-item[data-value="${selectedType}"]`);
-  
+  const switchBlock = document.querySelector(`.masterplan__switch`);
+
   selectedCheckbox.classList.add("active");
   selectedItem.classList.add("active");
-
-
-  switchItems.forEach((item) => {
+  const contentItems = document.querySelectorAll("g[data-type]");
+  switchItems.forEach((item, index) => {
     item.addEventListener("click", function () {
-      switchItems.forEach((el) => el.classList.remove("active"));
-      this.classList.add("active");
-      const selectedType = this.getAttribute("data-type");
-
-      contentItems.forEach((el) => el.classList.remove("active"));
-      document.querySelectorAll(`g[data-type="${selectedType}"]`).forEach((el) => {
-        el.classList.add("active");
-      });
-
-      if (selectedType === "exterior") {
-        checkboxBlock.classList.add("hide");
-      } else {
+      const currentType = item.getAttribute("data-type");
+      if (item.getAttribute("data-type") == "interior") {
+        switchBlock.classList.add("active");
         checkboxBlock.classList.remove("hide");
+      } else {
+        switchBlock.classList.remove("active");
+        checkboxBlock.classList.add("hide");
       }
+      contentItems.forEach((el) => {
+        if (el.getAttribute("data-type") === currentType) {
+          el.classList.add("active");
+        } else {
+          el.classList.remove("active");
+        }
+      });
     });
   });
 
+  let activeItem = selectedItem;
   checkboxItems.forEach((item) => {
     item.addEventListener("click", function () {
-      checkboxItems.forEach((el) => el.classList.remove("active"));
-      this.classList.add("active");
-      const selectedType = this.getAttribute("data-value");
-      checkboxValues.forEach((el) => el.classList.remove("active"));
-      document.querySelectorAll(`g[data-value="${selectedType}"]`).forEach((el) => {
-        el.classList.add("active");
+      const currentValue = item.getAttribute("data-value");
+      activeItem ? activeItem.classList.remove("active") : null;
+      item.classList.add("active");
+      activeItem = item;
+
+      checkboxValues.forEach((el) => {
+        if (el.getAttribute("data-value") === currentValue) {
+          el.classList.add("active");
+        } else {
+          el.classList.remove("active");
+        }
       });
     });
   });
